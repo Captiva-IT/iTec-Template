c11a6e84618844638a540e328efae8b572b35346
bennanizak33 <bennani.zakaria@outlook.com>
2019-10-13 22:41:59 +0200

    Adding js data file

---
+++ js/DataMock.js
@@ -0,0 +1,51 @@
+var livraisonData = [
+    { numeroBC: "0009213456", distributeur: "Martin BROWER", dateTransmission: "14/10/2019 à 00:00", dateConfirmation: "14/10/2019 à 00:00", numeroBl: "0045345", dateLivraison: "14/10/2019 à 00:00", status: "Validée" },
+    { numeroBC: "0009213457", distributeur: "Martin BROWER", dateTransmission: "14/10/2019 à 00:00", dateConfirmation: "14/10/2019 à 00:00", numeroBl: "0045345", dateLivraison: "14/10/2019 à 00:00", status: "Validée" },
+    { numeroBC: "0009213444", distributeur: "Martin BROWER", dateTransmission: "14/10/2019 à 00:00", dateConfirmation: "14/10/2019 à 00:00", numeroBl: "0045345", dateLivraison: "14/10/2019 à 00:00", status: "Validée" },
+    { numeroBC: "0009213423", distributeur: "Martin BROWER", dateTransmission: "14/10/2019 à 00:00", dateConfirmation: "14/10/2019 à 00:00", numeroBl: "0045345", dateLivraison: "14/10/2019 à 00:00", status: "Validée" },
+    { numeroBC: "0009213459", distributeur: "Martin BROWER", dateTransmission: "14/10/2019 à 00:00", dateConfirmation: "14/10/2019 à 00:00", numeroBl: "0045345", dateLivraison: "14/10/2019 à 00:00", status: "Validée" },
+    { numeroBC: "0009213423", distributeur: "Martin BROWER", dateTransmission: "14/10/2019 à 00:00", dateConfirmation: "14/10/2019 à 00:00", numeroBl: "0045345", dateLivraison: "14/10/2019 à 00:00", status: "Validée" },
+    { numeroBC: "0009213411", distributeur: "Martin BROWER", dateTransmission: "14/10/2019 à 00:00", dateConfirmation: "14/10/2019 à 00:00", numeroBl: "0045345", dateLivraison: "14/10/2019 à 00:00", status: "Validée" },
+];
+
+var LivraisonDetailData = [
+    { stockage: "2", code: "80000.673", designation: "ACCOMPAGN CAFE 2", qteColis: "500,00/CT", prop: "0", modification: "0", com: "0", type: "Libre", livJour: "0", stockTheorique: "NC", sfc: "NC", qte: "NC", inv: "", gamme: "TOUS", },
+    { stockage: "1", code: "00456.341", designation: "MELANGE SEL POIVRE", qteColis: "12,00/CT", prop: "NP", modification: "0", com: "0", type: "Libre", livJour: "0", stockTheorique: "NC", sfc: "NC", qte: "NC", inv: "", gamme: "TOUS", },
+    { stockage: "4", code: "12867.400", designation: "LIPTON GREEN TEA", qteColis: "180,00/CT", prop: "0", modification: "0", com: "0", type: "Libre", livJour: "0", stockTheorique: "0", sfc: "10/12/2013", qte: "0", inv: "", gamme: "NKF", },
+    { stockage: "2", code: "01239.450", designation: "COCA COLA", qteColis: "80,00/CT", prop: "0", modification: "0", com: "0", type: "Libre", livJour: "0", stockTheorique: "0", sfc: "NC", qte: "NC", inv: "", gamme: "NKF", },
+
+];
+
+var confirmationData = [
+    { numeroBC: "0009213456", distributeur: "Martin BROWER", type: "Marketing", dateTransmission: "14/10/2019 à 00:00", dateConfirmation: "14/10/2019 à 00:00" },
+    { numeroBC: "0009213457", distributeur: "Martin BROWER", type: "Marketing", dateTransmission: "14/10/2019 à 00:00", dateConfirmation: "14/10/2019 à 00:00" },
+    { numeroBC: "0009213444", distributeur: "Martin BROWER", type: "Hub", dateTransmission: "14/10/2019 à 00:00", dateConfirmation: "14/10/2019 à 00:00" },
+    { numeroBC: "0009213423", distributeur: "Martin BROWER", type: "Marketing", dateTransmission: "14/10/2019 à 00:00", dateConfirmation: "14/10/2019 à 00:00" },
+    { numeroBC: "0009213459", distributeur: "Martin BROWER", type: "Marketing", dateTransmission: "14/10/2019 à 00:00", dateConfirmation: "14/10/2019 à 00:00" },
+    { numeroBC: "0009213423", distributeur: "Martin BROWER", type: "Hub", dateTransmission: "14/10/2019 à 00:00", dateConfirmation: "14/10/2019 à 00:00" },
+    { numeroBC: "0009213411", distributeur: "Martin BROWER", type: "Marketing", dateTransmission: "14/10/2019 à 00:00", dateConfirmation: "14/10/2019 à 00:00" },
+];
+
+var confirmationDetailData = [
+    { stockage: "2", code: "80000.673", designation: "ACCOMPAGN CAFE 2", qteColis: "500,00/CT", commande: "12", confirme: "12", ecart: "0" },
+    { stockage: "1", code: "00456.341", designation: "MELANGE SEL POIVRE", qteColis: "12,00/CT", commande: "3", confirme: "3", ecart: "0" },
+    { stockage: "4", code: "12867.400", designation: "LIPTON GREEN TEA", qteColis: "180,00/CT", commande: "7", confirme: "7", ecart: "0" },
+    { stockage: "2", code: "01239.450", designation: "COCA COLA", qteColis: "80,00/CT", commande: "10", confirme: "10", ecart: "0" },
+];
+
+var commandeData = [
+    { numeroBC: "0009213456", distributeur: "Martin BROWER", type: "Marketing", aTransmettreAvant: "14/10/2019 à 00:00" },
+    { numeroBC: "0009213457", distributeur: "Martin BROWER", type: "Marketing", aTransmettreAvant: "14/10/2019 à 00:00" },
+    { numeroBC: "0009213444", distributeur: "Martin BROWER", type: "Hub", aTransmettreAvant: "14/10/2019 à 00:00" },
+    { numeroBC: "0009213423", distributeur: "Martin BROWER", type: "Marketing", aTransmettreAvant: "14/10/2019 à 00:00" },
+    { numeroBC: "0009213459", distributeur: "Martin BROWER", type: "Marketing", aTransmettreAvant: "14/10/2019 à 00:00" },
+    { numeroBC: "0009213423", distributeur: "Martin BROWER", type: "Hub", aTransmettreAvant: "14/10/2019 à 00:00" },
+    { numeroBC: "0009213411", distributeur: "Martin BROWER", type: "Marketing", aTransmettreAvant: "14/10/2019 à 00:00" },
+];
+
+var commandeDetailData = [
+    { stockage: "2", code: "80000.673", designation: "ACCOMPAGN CAFE 2", qteColis: "500,00/CT", prop: "0", modification: "0", com: "0", type: "Libre", livJour: "0", stockTheorique: "NC", sfc: "NC", qte: "NC", inv: "", gamme: "TOUS", },
+    { stockage: "1", code: "00456.341", designation: "MELANGE SEL POIVRE", qteColis: "12,00/CT", prop: "NP", modification: "0", com: "0", type: "Libre", livJour: "0", stockTheorique: "NC", sfc: "NC", qte: "NC", inv: "", gamme: "TOUS", },
+    { stockage: "4", code: "12867.400", designation: "LIPTON GREEN TEA", qteColis: "180,00/CT", prop: "0", modification: "0", com: "0", type: "Libre", livJour: "0", stockTheorique: "0", sfc: "10/12/2013", qte: "0", inv: "", gamme: "NKF", },
+    { stockage: "2", code: "01239.450", designation: "COCA COLA", qteColis: "80,00/CT", prop: "0", modification: "0", com: "0", type: "Libre", livJour: "0", stockTheorique: "0", sfc: "NC", qte: "NC", inv: "", gamme: "NKF", },
+];
