31d7150995c7bd720c55b4bb19b935fdf81b6ee5
bennanizak33 <bennani.zakaria@outlook.com>
2019-10-13 23:21:53 +0200

    js function Externalisation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+var msgIcon = function (cell, formatterParams) {
+    return cell.getValue() + ' <a href="#"data-toggle="modal" data-target="#modal"><i class="far fa-envelope"></i></a>';
+};
+
+var badge = function (cell, formatterParams, onRendered) {
+    var result;
+
+    switch (cell.getValue()) {
+        case 'Marketing':
+            result = "badge-secondary";
+            break;
+        case 'Hub':
+            result = "badge-primary";
+            break;
+        default:
+            result = "";
+    }
+    return '<span class="badge ' + result + '">' + cell.getValue() + '</span>';
+};
+
 $(function () {
     $('[data-toggle="popover"]').popover()
 })
